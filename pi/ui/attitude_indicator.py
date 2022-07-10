957cb6ab2a85f8a50ee87a19244bf1019732fa51
greyscale1ee <954887869@qq.com>
2022-07-10 20:41:07 +0800

    完成地面端姿态仪ui

---
+++ pi/ui/attitude_indicator.py
@@ -0,0 +1,32 @@
+from typing import Union
+import pygame
+import os
+
+class AttitudeIndicator:
+    def __init__(self, window_size: Union[tuple[2], list[2]], assets_path: str):
+        self.window_size = window_size
+        self.canvas = pygame.display.set_mode(window_size)
+
+        self.ai = pygame.image.load(os.path.join(assets_path, "att.png"))
+        self.ai_indicator_pitch = pygame.image.load(os.path.join(assets_path, "ai_indicator_pitch.png"))
+
+    def __del__(self):
+        pygame.quit()
+
+    def update(self, pitch: int, roll: int):
+        ai_crop = self.ai.subsurface(0, 360 - pitch * 4, self.ai.get_size()[0], self.ai.get_size()[1] - 720)
+        ai_rotate = pygame.transform.rotate(ai_crop, roll)
+        ai_indicator_pitch_rotate = pygame.transform.rotate(self.ai_indicator_pitch, roll)
+
+        ai_rotate_calib = ai_rotate.subsurface(ai_rotate.get_size()[0] // 2 - self.window_size[0] // 2,
+                                                ai_rotate.get_size()[1] // 2 - self.window_size[1] // 2,
+                                                self.window_size[0],
+                                                self.window_size[1])
+
+        self.canvas.blit(ai_rotate_calib, (0, 0))
+        self.canvas.blit(ai_indicator_pitch_rotate, (self.window_size[0] // 2 - ai_indicator_pitch_rotate.get_size()[0] // 2,
+                                            self.window_size[1] // 2 - ai_indicator_pitch_rotate.get_size()[1] // 2))
+        pygame.display.update()
+
+    def quit(self):
+        pygame.quit()
